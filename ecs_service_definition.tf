3db3619481c970519d5c56fe772f7bba9c4686de
Pelirrojo <Pelirrojo@users.noreply.github.com>
2025-02-19 11:39:03 +0100

    feat: Add lean ECS service definition for GPU tasks with auto-recovery

---
+++ ecs_service_definition.tf
@@ -0,0 +1,41 @@
+resource "aws_ecs_service" "gpu_service" {
+  name            = "gpu-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.gpu_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+
+  network_configuration {
+    subnets          = aws_subnet.private_subnet[*].id
+    security_groups  = [aws_security_group.ecs_tasks_sg.id]
+    assign_public_ip = false
+  }
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.instance-type == g4dn.xlarge"
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.ecs_asg_capacity_provider.name
+    weight           = 1
+    base            = 1
+  }
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
